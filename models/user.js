3b357d3d5809d79a7cd6295ee282c8a0cfdc23e8
biboibob <bobbyardian.work@gmail.com>
2022-04-14 23:16:22 +0700

    Routes Test Get And Post

---
+++ models/user.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define("user", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+}, {
+    //this representate which tabel on database
+    tableName: 'user'
+});
+
+return user; 
+};
