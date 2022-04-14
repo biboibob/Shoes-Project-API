c0c988b88ea8d4d43b5fa1cea0a1a8f584c85c09
biboibob <bobbyardian.work@gmail.com>
2022-04-14 23:20:38 +0700

    revert user.js and remove function relevant with fastest validator

---
+++ routes/user.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var router = express.Router();
+
+const Validator = require("fastest-validator");
+
+const { user } = require("../models");
+
+const v = new Validator();
+
+// router.post("/", async (req, res, next) => {
+//   const schema = {
+//     id: "number|optional",
+//     name: "string",
+//     role: "string",
+//   };
+
+//   const validate = v.validate(req.body, schema);
+
+//   if (validate.length) {
+//     return res.status(400).json(validate);
+//   } else {
+//     const userAdd = await user.create(req.body);
+//     res.json(userAdd);
+//   }
+// });
+
+/* GET users listing. */
+router.get("/", async (req, res, next) => {
+  const data = await user.findAll();
+  return res.json(data);
+});
+
+module.exports = router;
