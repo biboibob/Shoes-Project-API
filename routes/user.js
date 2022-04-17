e5bbedf41c8fc2c37b89addac54c53bf02c02bd7
biboibob <bobbyardian.work@gmail.com>
2022-04-18 00:14:16 +0700

    Add Login Logic

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,7 @@
+var bcrypt = require("bcryptjs");
+var saltRounds = bcrypt.genSaltSync(10);
+
+const sequelize = require("sequelize");
 var express = require("express");
 var router = express.Router();
 const Validator = require("fastest-validator");
@@ -29,9 +33,52 @@ router.post("/", async (req, res, next) => {
 router.get("/", async (req, res, next) => {
   const data = await user.findAll();
   res.json(data);
-   // res.json({ message: "hi im in!"});
+  // res.json({ message: "hi im in!"});
 });
 
+router.post("/auth", async (req, res, next) => {
+  const schema = {
+    id: "number|optional",
+    username: "string",
+    password: "string",
+  };
 
+  const validate = v.validate(req.body, schema);
+
+  if (validate.length) {
+    return res.status(400).json(validate);
+  } else {
+    const { username, password } = req.body;
+    const data = await user.findOne({
+      where: sequelize.where(
+        sequelize.fn("BINARY", sequelize.col("username")),
+        username
+      ),
+    });
+
+    if (data === null) {
+      res.json({
+        status: 400,
+        content: "Your Username Doesn't Exist",
+        data: [],
+      });
+    } else {
+      const compareHash = bcrypt.compareSync(password, data.password);
+      if (!compareHash) {
+        res.json({
+          status: 400,
+          content: "Your Password Wrong",
+          data: [],
+        });
+      } else {
+        res.json({
+          status: 200,
+          content: "Welcome",
+          data: data,
+        });
+      }
+    }
+  }
+});
 
 module.exports = router;
