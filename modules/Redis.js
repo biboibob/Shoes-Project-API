4bb2cbd6e0bd617e02d81892e0b557d1da6b21c7
bobby ardian <bobbyardian.work@gmail.com>
2022-06-03 08:54:37 +0700

    Git Upstash Redis

--- modules/Redis.js
+++ modules/Redis.js
@@ -15,14 +15,17 @@ require("dotenv").config();
 
 // const redisCon = redisClient(6379, "localhost");
 
-// const redis = require("redis");
-const Redis = require("ioredis");
-const redisClient = new Redis(process.env.REDIS_URL);
+// const Redis = require("ioredis");
+// const redisClient = new Redis(process.env.REDIS_URL);
+
+const { Redis } = require("@upstash/redis");
+const redis = Redis.fromEnv()
+// const redisClient = new Redis("rediss://:39748f796ecc4f5088c91d516adba6ad@global-above-grouse-30335.upstash.io:30335");
 
 //get Redis Cache
 function get(redis_key) {
   return new Promise(async (resolve) => {
-    redisClient.get(redis_key, (err, reply) => {
+    redis.get(redis_key, (err, reply) => {
       if (err) {
         console.log("Redis Conn Error", err);
       } else {
@@ -35,7 +38,7 @@ function get(redis_key) {
 
 function set(redis_key, redis_value) {
   return new Promise(async (resolve) => {
-    redisClient.set(redis_key, redis_value, (err, reply) => {
+    redis.set(redis_key, redis_value, (err, reply) => {
       if (err) {
         console.log("Redis Conn Error", err);
       } else {
