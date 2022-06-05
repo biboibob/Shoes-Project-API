29a4aa336f75f5e2b5372c14b8df8153fedef304
bobby ardian <bobbyardian.work@gmail.com>
2022-06-05 20:36:38 +0700

    first commit

--- routes/authServer.js
+++ routes/authServer.js
@@ -114,7 +114,7 @@ router.delete("/logout", async (req, res, next) => {
 
 function generateAccessToken(userInfo) {
   return jwt.sign(userInfo, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "10s",
+    expiresIn: "5s",
   });
 }
 
